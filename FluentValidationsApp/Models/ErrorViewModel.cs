566c6444266ffbe65396f032e5fecfbe9c0a1340
yavuz <yavuz@farmazon.com.tr>
2020-06-07 23:16:02 +0300

    Add project files.

---
+++ FluentValidationsApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FluentValidationsApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
