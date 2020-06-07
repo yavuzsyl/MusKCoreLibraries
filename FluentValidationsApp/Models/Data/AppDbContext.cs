2976eea2420aa793d77ba168ff73a8a47607087a
yavuz <yavuz@farmazon.com.tr>
2020-06-07 23:50:16 +0300

    Intro getting ready the project

---
+++ FluentValidationsApp/Models/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentValidationsApp.Models.Data
+{
+    public class AppDbContext : DbContext
+    {
+
+        public DbSet<Customer> Customers { get; set; }
+
+        //options ile bu context.cs de kullanılacak sql provider belirtilecek ve varsa ek ayarlar
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- FluentValidationsApp/Startup.cs
+++ FluentValidationsApp/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentValidationsApp.Models.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,12 @@ namespace FluentValidationsApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("FluentDbCon"));
+                //options.UseSqlServer(Configuration["FluentDbCon"]);
+            
+            });
             services.AddControllersWithViews();
         }
 
